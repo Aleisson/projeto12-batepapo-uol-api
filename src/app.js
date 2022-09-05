c6baf60b4fa57477b6536207ef2f6046896819e7
Aleisson <caioaleisson@gmail.com>
2022-09-05 02:02:00 -0300

    finalizado

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import { json } from "express";
 import cors from "cors";
 import dotnev from "dotenv";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dayjs from "dayjs";
 import Joi from 'joi';
 
@@ -16,16 +16,12 @@ app.use(json());
 // - mongo
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 let database;
-mongoClient.connect(() => {
+mongoClient.connect( () => {
     database = mongoClient.db("driven");
 });
 
-app.get("/", (req, res) => {
-
-    res.send("<h1>Api Batepapo UOL</h1>")
 
 
-})
 
 // - validação
 
@@ -35,15 +31,22 @@ const participantSchema = Joi.object({
 const messageSchema = Joi.object({
     to: Joi.string().required(),
     text: Joi.string().required(),
-    type: Joi.string().valid( 'message','private_message').required()
-    
+    type: Joi.string().valid('message', 'private_message').required()
+
+})
+
+app.get("/", (req, res) => {
+
+    res.send("<h1>Api Batepapo UOL</h1>")
+
+
 })
 
 // - /participants
 
 app.post("/participants", async (req, res) => {
 
-    
+
     // se tiver desconstruido
     const valida = participantSchema.validate(req.body);
 
@@ -52,7 +55,7 @@ app.post("/participants", async (req, res) => {
         return;
     }
 
-    const {name} = req.body;
+    const { name } = req.body;
 
     try {
         const checkName = await database.collection("participants").findOne({ name });
@@ -77,6 +80,7 @@ app.get("/participants", async (req, res) => {
 
     try {
         const participants = await database.collection("participants").find().toArray();
+        
         res.send(participants);
     } catch (error) {
         console.error(error);
@@ -91,32 +95,32 @@ app.get("/participants", async (req, res) => {
 app.post("/messages", async (req, res) => {
 
     const { from } = req.headers;
-    
 
-    const valida = messageSchema.validate(req.body) 
+
+    const valida = messageSchema.validate(req.body)
 
     // ver se precisa também validar type e from
     if (valida.error) {
-        valida.error.details.map( x => console.log(x.message));
-       
+        valida.error.details.map(x => console.log(x.message));
+
         res.sendStatus(422);
         return;
     }
 
-    
+
 
     const { to, text, type } = req.body;
 
     try {
         const checkName = await database.collection("participants").findOne({ name: from });
-        //console.log("checkname:" + checkName.name);
+        //console.log("checkname:" + checkName);
         if (!checkName) {
             res.sendStatus(422);
             return;
         }
 
 
-        await database.collection("messages").insertOne({ from, to, text,  type, time: dayjs().format("HH:mm:ss") })
+        await database.collection("messages").insertOne({ from, to, text, type, time: dayjs().format("HH:mm:ss") })
         res.sendStatus(201);
     } catch (error) {
         console.error(error);
@@ -136,16 +140,16 @@ app.get("/messages", async (req, res) => {
     console.log(user);
     try {
         //filtrar aqui
-       const messages = await database.collection("messages").find().toArray();
-       
-       const  messagesFiltradas = [...messages].filter( message => message.to === "Todos" || message.to === user || message.from === user).reverse()
-       
-       if (limit) {
-            res.status(200).send(messagesFiltradas.splice(0,limit));
+        const messages = await database.collection("messages").find().toArray();
+
+        const messagesFiltradas = [...messages].filter(message => message.to === "Todos" || message.to === user || message.from === user).reverse()
+
+        if (limit) {
+            res.status(200).send(messagesFiltradas.splice(0, limit).reverse());
             return;
         }
 
-        res.status(200).send(messagesFiltradas.slice(0,100));
+        res.status(200).send(messagesFiltradas.slice(0, 100).reverse());
 
     } catch (error) {
         console.error(error);
@@ -158,14 +162,65 @@ app.get("/messages", async (req, res) => {
 
 // - /status
 
-app.get("/status", (req, res) => {
+app.post("/status", async (req, res) => {
 
     const { user } = req.headers;
+    //console.log(user)
+    try {
+        const userStatus = await database.collection("participants").findOne({ name: user});
+        //console.log(userStatus);
 
-    console.log(nome);
+        if (!userStatus) {
+            res.sendStatus(404);
+            return;
+        }
+
+        await database.collection("participants").updateOne({_id: userStatus._id},{$set:{lastStatus: Date.now()}})
+
+    } catch (error) {
+
+    }
 
     res.sendStatus(200);
 
 })
+
+// - functions
+
+async function testStatus(participant){
+    try {
+        if((Date.now() - participant.lastStatus) > 15000){
+            await database.collection('participants').deleteOne({_id: ObjectId(participant._id)})
+            await database.collection("messages").insertOne({ from: participant.name, to: "Todos", text: "sai na sala...", type: "status", time: dayjs().format('HH:mm:ss') })
+        }
+    
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+ 
+}
+
+
+async function removeParticipants(){
+
+    
+
+    try {
+        const participants = await database.collection("participants").find().toArray();
+        console.log(participants);
+        participants.forEach(participant => testStatus(participant));
+        console.log("aqui")
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+
+   
+
+}
+
+// setInterval(removeParticipants(), 15000);
+
 
 app.listen(5000, () => console.log("Porta 5000"));
